fc264ff188008601f06e48bc3663ed4e5a4e837e
Mak655 <mekdemyoseph@gmail.com>
2022-07-01 09:49:34 -0700

    king

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
+ * main - Prints reversed alphabet.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low;
+ int i;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+ for (i = 122; i > 96; i--)
+ {
+ putchar(i);
+ }
+ putchar('\n');
+return (0);
 }

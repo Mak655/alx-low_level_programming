42403fe627ca6360a1f99a06060ec7e7616d0999
Mak655 <mekdemyoseph@gmail.com>
2022-10-03 09:53:25 -0700

    king

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/ermiasbahru/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
-export LD_PRELOAD=/tmp/winwin.so
+gcc -c -Wall -Werror -fPIC *.c
+gcc -shared -o liball.so *.o
